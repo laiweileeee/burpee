d91000b23ad420a48e419b8714d0d0f83c4bb55b
Lai Wei Lee <laiwei>
2020-06-29 13:38:57 +0800

    Updates

---
+++ precache-manifest.7b2a8019cb4aa5f7950089712d0c4540.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d385dddc749afddb63ba0867cbb8ef7c",
+    "url": "/burpee/index.html"
+  },
+  {
+    "revision": "a9cf996e29148a0adec2",
+    "url": "/burpee/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "34c23ad94d9954f49404",
+    "url": "/burpee/static/css/main.640e370b.chunk.css"
+  },
+  {
+    "revision": "a9cf996e29148a0adec2",
+    "url": "/burpee/static/js/2.522ae5d8.chunk.js"
+  },
+  {
+    "revision": "6e4733f8889d483c0478280e611387dd",
+    "url": "/burpee/static/js/2.522ae5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c23ad94d9954f49404",
+    "url": "/burpee/static/js/main.bf162113.chunk.js"
+  },
+  {
+    "revision": "17abf0e2a9181a75a57c",
+    "url": "/burpee/static/js/runtime-main.73f3f2da.js"
+  },
+  {
+    "revision": "5b2413002bd40db3549d89c415101371",
+    "url": "/burpee/static/media/card_back9.5b241300.png"
+  }
+]);
